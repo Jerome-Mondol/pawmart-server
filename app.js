abcf99ba53230faacebdc9ebfbaff336a103fc70
Jerome Mondol <jeromemondol11@gmail.com>
2025-11-09 23:53:26 +0600

    users post route added

--- app.js
+++ app.js
@@ -18,6 +18,7 @@ const client = new MongoClient(uri, {
 });
 
 let petListingsCollection;
+let db;
 
 app.use(cors());
 app.use(express.json());
@@ -30,10 +31,13 @@ app.get('/', (req, res) => {
 app.get('/pets', async (req, res) => {
     try {
         if(!petListingsCollection) return res.status(500).json({ message: "Database not ready yet!" })
+
         let query = petListingsCollection.find({}).sort({ date: -1 });
         const count = req.query.count ? parseInt(req.query.count) : null;
+
         if(count) query = query.limit(count);
         const listings = await query.toArray();
+
         res.status(200).json(listings)
     }
     catch(err) {
@@ -41,26 +45,49 @@ app.get('/pets', async (req, res) => {
     }
 })
 
+app.post('/users', async (req, res) => {
+  try {
+    const {displayName, photoURL, email } = req.body;
+  if(!email || !displayName) {
+    return res.status(400).json({ message: "Missing requires fields" })
+  }
+
+  const usersCollections = db.collection('users');
+  const existingUser = await usersCollections.findOne({ email })
+  if(existingUser) {
+    return res.status(409).json({ message: "user already exists" })
+  }
+
+  const newUser = {email, displayName, photoURL, createdAt: new Date()}
+  await usersCollections.insertOne(newUser);
+  res.status(201).json({ message: 'User saved successfully', user: newUser });
+  }
+  catch(err) {
+    console.log(err);
+        res.status(500).json({ message: 'Server error' });
+  }
+})
+
+
+
+
 async function startDB() {
   try {
     await client.connect();
-    const db = client.db('pawMart')
+    db = client.db('pawMart')
     petListingsCollection = db.collection('petListings')
 
 
     app.listen(port, () => {
         console.log(`Server running on port ${port}`)
     })
-  } 
+  }
   catch(err) {
     console.error(err);
   }
 }
 
 startDB();
-
-
-
 
 
 
